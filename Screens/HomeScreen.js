174f395627cb6ddb29a29037c9a1c292f96bff5d
petercolella <peter.joseph.colella@gmail.com>
2018-02-07 02:38:10 -0500

    added styles to homescreen

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -9,7 +9,7 @@ import {
 class HomeScreen extends Component {
     render(){
         return (
-            <View>
+            <View style={styles.container}>
                 <Text>Welcome to unTurnd!</Text>
                 <Button onPress = { () =>
                 this.props.navigation.navigate('LoginScreen')}
@@ -18,5 +18,14 @@ class HomeScreen extends Component {
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+});
 
 export default HomeScreen;
