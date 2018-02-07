abf58960433c3d8de4c95a04b6946e4c64564ef9
petercolella <peter.joseph.colella@gmail.com>
2018-02-07 02:17:50 -0500

    installed stack navigator, created home and login screens

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {StackNavigator} from 'react-navigation'
+
+import LoginScreen from './Screens/LoginScreen'
+import HomeScreen from './Screens/HomeScreen'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Welcome to unTurnd!</Text>
-      </View>
+      // <View style={styles.container}>
+      //   <Text>Welcome to unTurnd!</Text>
+      // </View>
+      <AppNavigator/>
     );
   }
 }
+
+const AppNavigator = StackNavigator({
+  LoginScreen: { screen: LoginScreen },
+  HomeScreen: { screen: HomeScreen }
+})
 
 const styles = StyleSheet.create({
   container: {
---
+++ Screens/LoginScreen.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet
+} from 'react-native';
+
+class LoginScreen extends Component {
+    render(){
+        return (
+            <View>
+                <Text>Login</Text>
+            </View>
+        );
+    }
+}
+
+export default LoginScreen;
